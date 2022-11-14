bd80b485b5dd837401f86ec9eba4e846966f9b09
Milad <milad.beigi0098@gmail.com>
2022-11-15 00:13:04 +0330

    add Vertical line

--- src/Components/Containers/Containers.js
+++ src/Components/Containers/Containers.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Description from "../Descriptions/Descriptions";
 import Background from "../Background/Background";
+import Line from "../Line/Line";
 import "./Containers.css";
 
 const Containers = () => {
   return (
     <div className="Containers">
+      <Line/>
       <Description />
       <Background />
     </div>
---
+++ src/Components/Line/Line.js
@@ -0,0 +1,6 @@
+import React from "react";
+import './Lins.css'
+const Line =()=>{
+      return <div className="line"><h4>s</h4></div>
+}
+export default Line
